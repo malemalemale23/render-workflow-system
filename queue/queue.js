ef0b57305c90c6cde3f41514d72c1c76d7efefa4
malemalemale23 <malemale2323@hotmail.com>
2026-04-24 03:23:35 +0700

    update

--- queue/queue.js
+++ queue/queue.js
@@ -1,8 +1,6 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
-const connection = new IORedis(process.env.REDIS_URL);
-
 export const workflowQueue = new Queue("workflow", {
-  connection,
+  connection: new IORedis(process.env.REDIS_URL)
 });
